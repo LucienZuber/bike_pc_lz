0992c3dc271092c4cb89cd839d1ec4c2491b876c
clivazpat <clivaz.patrick@icloud.com>
2017-11-07 17:02:38 +0100

    More translations

--- languages/german.php
+++ languages/german.php
@@ -5,3 +5,55 @@
  * Date: 07.11.17
  * Time: 15:56
  */
+
+$lang = array();
+
+$lang['CONNECT'] = 'Connect now';
+$lang['NAME'] = 'Name';
+$lang['PASSWORD'] = 'Password';
+$lang['LOGIN'] = 'Login';
+$lang['LOGOUT'] = 'Logout';
+$lang['START'] = 'Departure';
+$lang['END'] = 'Arrival';
+$lang['DATE'] = 'Date';
+$lang['TIME'] = 'Time';
+$lang['CONFIRM'] = 'Confirm';
+$lang['START_TIME'] = 'Departure Time';
+$lang['END_TIME'] = 'Arrival Time';
+$lang['COMPLETE_RESERVATION'] = 'Complete a reservation';
+$lang['PHONE'] = 'Phone';
+$lang['MAIL'] = 'Mail';
+$lang['NB_BIKE'] = 'Number of Bikes';
+$lang['CANCEL'] = 'Cancel';
+$lang['LIST_BOOKINGS'] = 'List of Bookings';
+$lang['FROM'] = 'From';
+$lang['TO'] = 'To';
+$lang['ADD_RESERVATION'] = 'Add a reservation';
+$lang['IMPORT_ZONES'] = 'Add new zones';
+$lang['REGION'] = 'Region';
+$lang['CHOOSE'] = 'Choose';
+$lang['ADMIN'] = 'Administrator';
+$lang['IMPORT_REGION'] = 'Import a region';
+$lang['ADD_REGION'] = 'Add a region';
+$lang['LIST_USERS'] = 'List of Users';
+$lang['ROLE'] = 'Role';
+$lang['STATION'] = 'Station';
+$lang['ADD_TRIP'] = 'Add a trip';
+$lang['IMPORT_STATION_REGION'] = 'Import new stations to an existing region';
+$lang['ADD_USER'] = 'Add a User';
+
+
+// Menu
+$lang['MENU_BOOK'] = 'Book';
+$lang['MENU_BOOKINGS'] = 'Bookings';
+$lang['MENU_MANAGE_USERS'] = 'Manage Users';
+$lang['MENU_MANAGE_REGIONS'] = 'Manage Regions';
+
+// Footer
+$lang['FOOTER_CREDITS'] = 'Made with ❤︎ by Patrick Clivaz & Lucien Zuber.';
+$lang['FOOTER_LANGUAGES'] = 'Change Language';
+$lang['LANG_DEFAULT'] = 'English';
+$lang['LANG_FRENCH'] = 'French';
+$lang['LANG_GERMAN'] = 'German';
+
+?>
