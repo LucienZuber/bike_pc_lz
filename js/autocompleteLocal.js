afbb962caebba91e2a046ddd8b71aa31721efe1e
finalcondom <EprVblMpG1T>
2017-11-08 14:37:48 +0100

    autocomplete fix

--- js/autocompleteLocal.js
+++ js/autocompleteLocal.js
@@ -3,9 +3,6 @@ $(document).ready(function() {
         var stations = {};
         var input = $(this).val();
         getData(input, stations);
-        $('input.rechercheDB').change(function() {
-            $(this).val(Object.keys(stations)[0]);
-        })
     })
     Materialize.updateTextFields();
 })
@@ -25,9 +22,6 @@ function getData(input, stations) {
             $('input.rechercheDB').autocomplete({
                 data: stations,
                 limit: 10, // The max amount of results that can be shown at once. Default: Infinity.
-                onAutocomplete: function(val) {
-                    // Callback function when value is autocompleted.
-                },
                 minLength: 2, // The minimum length of the input for the autocomplete to start. Default: 1.
             });
         }
